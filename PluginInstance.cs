2e47ed3e6f2b06514ae1306fa4c82679e4cfc0c8
Cmmmmmm <512065439@qq.com>
2025-05-26 16:40:00 +0100

    init: PluginInstance.cs

---
+++ PluginInstance.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RecipeHint;
+
+public abstract class PluginInstance<T>: MonoBehaviour where T : MonoBehaviour{
+    public static T Instance;
+
+    protected virtual void Awake() {
+        PluginInstance<T>.Instance = this as T;
+        Plugin.Logger.LogInfo($"PluginInstance<{typeof(T).Name}> initialized.");
+    }
+
+    protected virtual void OnDestroy() {
+        Object.Destroy(base.gameObject);
+    }
+    
+}
