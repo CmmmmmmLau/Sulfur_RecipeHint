e9ec1772a9344f0d6742e5019e7aaf6be6bd45ae
Cmmmmmm <512065439@qq.com>
2025-06-22 19:27:40 +0100

    add some assets

---
+++ Patcher/CraftingManagerPatch.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using PerfectRandom.Sulfur.Core;
+using RecipeHint;
+using RecipeHint.Components;
+
+[HarmonyWrapSafe]
+[HarmonyPatch(typeof(CraftingManager), "Start")]
+public class CraftingManagerPatch {
+    private static void Postfix(CraftingManager __instance) {
+        PluginInstance<RecipeManager>.Instance.CacheRecipe(__instance);
+    }
+}
